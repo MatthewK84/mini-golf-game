5eae166ddb6807dc50055d6c44844481574f8ab4
Matthew Kolakowski <62799075+MatthewK84@users.noreply.github.com>
2025-02-26 21:12:03 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   root: '.',           // the root directory of your project
   base: './',          // use relative paths
   build: {
-    outDir: 'dist',    // your build output folder
+    outDir: 'dist/client',  // updated to match Netlify publish directory
   },
   server: {
     open: true         // automatically open your app in the browser
